9a8b65ce8ee399163a4c1ba5d5eca78a766f8b01
Nicolas Eeckeloo <neeckeloo@gmail.com>
2016-04-07 14:55:09 +0200

    Minor update

--- src/Factory/GuardListenerFactory.php
+++ src/Factory/GuardListenerFactory.php
@@ -3,12 +3,13 @@ namespace Riskio\Recurly\NotificationModule\Factory;
 
 use Interop\Container\ContainerInterface;
 use Riskio\Recurly\NotificationModule\Listener\GuardListener;
+use Riskio\Recurly\NotificationModule\Specification\IsNotificationEvent;
 
 class GuardListenerFactory
 {
     public function __invoke(ContainerInterface $serviceLocator) : GuardListener
     {
-        $isNotificationEvent = $serviceLocator->get('Riskio\Recurly\NotificationModule\Specification\IsNotificationEvent');
+        $isNotificationEvent = $serviceLocator->get(IsNotificationEvent::class);
         $config = $serviceLocator->get('Riskio\Recurly\NotificationModule\Config');
 
         $guards = [];
