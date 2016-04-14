cff2f6b07ad6fa16164651a0263c08cc68a0389a
Nicolas Eeckeloo <neeckeloo@gmail.com>
2016-04-14 23:25:31 +0200

    Fix config file path

--- src/Module.php
+++ src/Module.php
@@ -34,6 +34,6 @@ class Module
 
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 }
